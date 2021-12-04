7bafdd64ec5d7087084449fe323048bb23c14f82
eunjin <jklkm90@gmail.com>
2021-12-04 22:09:07 +0900

    입력 값 검증

--- src/main/java/kr/co/dpm/system/access/AccessController.java
+++ src/main/java/kr/co/dpm/system/access/AccessController.java
@@ -2,12 +2,14 @@ package kr.co.dpm.system.access;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 public class AccessController {
@@ -22,12 +24,19 @@ public class AccessController {
 
     // 로그인
     @PostMapping("/login")
-    public ModelAndView login(User user, HttpSession httpSession) {
+    public ModelAndView login(@Valid User user, Errors errors, HttpSession httpSession) {
         ModelAndView mav = null;
+
+        if (errors.hasErrors()) {
+            mav = new ModelAndView("access/login");
+
+            errors.reject("user", "로그인 정보를 입력하세요.");
+            return mav;
+        }
 
         User userInfo = userRepository.select();
         if (user.getId().equals(userInfo.getId())
-                && user.getPassword().equals(userInfo.getPassword())){
+                && user.getPassword().equals(userInfo.getPassword())) {
             httpSession.setAttribute("log", user.getId());
             mav = new ModelAndView(new RedirectView("/devices"));
         } else {
