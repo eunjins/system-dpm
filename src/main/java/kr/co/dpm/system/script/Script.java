21e640bc9e645ea8f1b0b6db50caf2b1fcbca5c2
eunjin <jklkm90@gmail.com>
2021-12-05 12:43:06 +0900

    코드 정리

--- src/main/java/kr/co/dpm/system/script/Script.java
+++ src/main/java/kr/co/dpm/system/script/Script.java
@@ -33,13 +33,4 @@ public class Script implements Serializable {
     public void setUploadPoint(String uploadPoint) {
         this.uploadPoint = uploadPoint;
     }
-
-    @Override
-    public String toString() {
-        return "Script{" +
-                "no=" + no +
-                ", name='" + name + '\'' +
-                ", uploadPoint='" + uploadPoint + '\'' +
-                '}';
-    }
 }
