13fd1e6560d6b7d758bd74600bad7bf9e7a50ea0
ghdgh <ghdghd5560@gmail.com>
2021-12-07 15:01:41 +0900

    코드 프로세스와 일치화 및 경량화

--- src/main/java/kr/co/dpm/system/script/ScriptFileRepository.java
+++ src/main/java/kr/co/dpm/system/script/ScriptFileRepository.java
@@ -6,5 +6,5 @@ import java.io.File;
 
 @Repository
 public interface ScriptFileRepository {
-    public boolean distribute(File classFile, String encryptResult, String url) throws Exception;
+    public boolean distribute(File classFile, String encryptResult, String url);
 }
